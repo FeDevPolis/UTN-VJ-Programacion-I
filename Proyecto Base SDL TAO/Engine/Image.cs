abac19f100f6f2ba68ecd91b496894e7b98b51bc
Federico Poliseno <federicopoliseno@yahoo.com.ar>
2024-10-03 00:00:43 -0300

    SDL TAO Base Project

---
+++ Proyecto Base SDL TAO/Engine/Image.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Tao.Sdl;
+
+public class Image
+{
+    public IntPtr Pointer { get; private set; }
+
+    public Image(string imagePath)
+    {
+        LoadImage(imagePath);
+    }
+
+    private void LoadImage(string imagePath)
+    {
+        Pointer = SdlImage.IMG_Load(imagePath);
+        if (Pointer == IntPtr.Zero)
+        {
+            Console.WriteLine("Imagen inexistente: {0}", imagePath);
+            Environment.Exit(4);
+        }
+    }
+}
---
+++ Proyecto Base SDL TAO/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Media;
+using Tao.Sdl;
+
+namespace MyGame
+{
+    
+    class Program
+    {
+        
+        static Image image = Engine.LoadImage("assets/fondo.png");
+        
+        
+        static void Main(string[] args)
+        {
+            Engine.Initialize();
+            
+            
+            while (true)
+            {
+                
+                CheckInputs();
+                Update();
+                Render();
+                Sdl.SDL_Delay(20);  
+            }
+        }
+
+        static void CheckInputs()
+        {
+            if (Engine.KeyPress(Engine.KEY_LEFT))
+            {
+
+            }
+
+            if (Engine.KeyPress(Engine.KEY_LEFT))
+            {
+
+            }
+
+            if (Engine.KeyPress(Engine.KEY_LEFT))
+            {
+
+            }
+
+            if (Engine.KeyPress(Engine.KEY_LEFT))
+            {
+
+            }
+
+            if (Engine.KeyPress(Engine.KEY_ESC))
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        static void Update()
+        {
+        
+        }
+
+        static void Render()
+        {
+            Engine.Clear();
+            Engine.Draw(image, 0, 0);
+            Engine.Show();
+        }
+    }
+}
